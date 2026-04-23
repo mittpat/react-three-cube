343e7c7788f868923c19a406b1a6ecae439085ba
Patrick Pelletier <mitthrawn@hotmail.com>
2026-04-22 21:54:12 -0400

    Simplify Charizard rotation

--- src/components/SpinningCube.jsx
+++ src/components/SpinningCube.jsx
@@ -1,39 +1,15 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
-import * as THREE from 'three'
 
 export default function SpinningCube() {
   const groupRef = useRef(null)
-  const { scene, animations } = useGLTF('/charizard_sv_v3.glb')
-  const mixerRef = useRef(null)
+  const { scene } = useGLTF('/charizard_sv_v3.glb')
 
-  useEffect(() => {
-    console.log('GLB loaded. Animations found:', animations.length)
-    console.log('Animation names:', animations.map(a => a.name))
-
-    if (scene) {
-      const mixer = new THREE.AnimationMixer(scene)
-      mixerRef.current = mixer
-
-      if (animations.length > 0) {
-        const action = mixer.clipAction(animations[0])
-        console.log('Playing animation:', animations[0].name)
-        action.play()
-      } else {
-        console.log('No animations found in model')
-      }
-    }
-
-    return () => {
-      mixerRef.current?.stopAllAction()
-    }
-  }, [scene, animations])
-
-  useFrame((state, delta) => {
-    mixerRef.current?.update(delta)
+  useFrame(() => {
     if (groupRef.current) {
-      groupRef.current.rotation.y += 0.005
+      groupRef.current.rotation.y += 0.01
+      groupRef.current.rotation.x += 0.002
     }
   })
 
